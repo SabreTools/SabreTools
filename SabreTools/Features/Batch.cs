869b4461969c584099d01c01964f516fd5a424be
Matt Nadareski <mnadareski@outlook.com>
2020-08-26 14:07:00 -0700

    Better wording

--- SabreTools/Features/Batch.cs
+++ SabreTools/Features/Batch.cs
@@ -24,7 +24,7 @@ namespace SabreTools.Features
             Flags = new List<string>() { "-bt", "--batch" };
             Description = "Enable batch mode";
             _featureType = FeatureType.Flag;
-            LongDescription = @"Run a special mode that takes input files as lists of batch commands to run sequentially as read in from an input file. Multiple batch files are allowed and each command has to be its own line.
+            LongDescription = @"Run a special mode that takes input files as lists of batch commands to run sequentially. Each command has to be its own line and must be followed by a semicolon (`;`). Multiple batch files are allowed but they will be run independently from each other.
 
 The following commands are currently implemented:
 
