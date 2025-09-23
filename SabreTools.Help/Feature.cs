d6446bec2516f917e69cd38222bbfb2756ff42ab
Matt Nadareski <mnadareski@outlook.com>
2025-09-23 17:04:03 -0400

    Split this file in half

--- SabreTools.Help/Feature.cs
+++ SabreTools.Help/Feature.cs
@@ -394,36 +394,4 @@ namespace SabreTools.Help
 
         #endregion
     }
-
-    public abstract class Feature<T> : Feature
-    {
-        public T? Value { get; protected set; }
-
-        #region Constructors
-
-        internal Feature(string name, string flag, string description, string? longDescription = null)
-            : base(name, flag, description, longDescription)
-        {
-        }
-
-        internal Feature(string name, string[] flags, string description, string? longDescription = null)
-            : base(name, flags, description, longDescription)
-        {
-        }
-
-        #endregion
-
-        #region Instance Methods
-
-        /// <inheritdoc/>
-        public override abstract bool ValidateInput(string input, bool exact = false, bool ignore = false);
-
-        /// <inheritdoc/>
-        public override abstract bool IsEnabled();
-
-        /// <inheritdoc/>
-        protected override abstract string FormatFlags();
-
-        #endregion
-    }
 }
