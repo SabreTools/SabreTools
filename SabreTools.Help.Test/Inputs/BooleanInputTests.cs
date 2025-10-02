3b507d4af70bbc914aa4d03bbc88f5b1db363fdd
Matt Nadareski <mnadareski@outlook.com>
2025-10-01 21:44:24 -0400

    Add tests for all Help input types

--- SabreTools.Help.Test/Inputs/BooleanInputTests.cs
+++ SabreTools.Help.Test/Inputs/BooleanInputTests.cs
@@ -1,7 +1,134 @@
-namespace SabreTools.Help.Test
+using SabreTools.Help.Inputs;
+using Xunit;
+
+namespace SabreTools.Help.Test.Inputs
 {
     public class BooleanInputTests
     {
+        [Fact]
+        public void ProcessInput_EmptyArgs_Failure()
+        {
+            string[] args = [];
+            int index = 0;
 
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.False(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_NegativeIndex_Failure()
+        {
+            string[] args = ["a", "true"];
+            int index = -1;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(-1, index);
+            Assert.False(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_OverIndex_Failure()
+        {
+            string[] args = ["a", "true"];
+            int index = 2;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(2, index);
+            Assert.False(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Space_InvalidLength_Failure()
+        {
+            string[] args = ["a"];
+            int index = 0;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.False(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Space_InvalidValue_Failure()
+        {
+            string[] args = ["a", "ANY"];
+            int index = 0;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.False(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Space_ValidValue_Success()
+        {
+            string[] args = ["a", "true"];
+            int index = 0;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.True(actual);
+            Assert.Equal(1, index);
+            Assert.True(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Equal_InvalidLength_Failure()
+        {
+            string[] args = ["a="];
+            int index = 0;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.False(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Equal_InvalidValue_Failure()
+        {
+            string[] args = ["a=ANY"];
+            int index = 0;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.False(input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Equal_ValidValue_Success()
+        {
+            string[] args = ["a=true"];
+            int index = 0;
+
+            var input = new BooleanInput("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.True(actual);
+            Assert.Equal(0, index);
+            Assert.True(input.Value);
+        }
     }
 }
--- SabreTools.Help.Test/Inputs/Int8InputTests.cs
+++ SabreTools.Help.Test/Inputs/Int8InputTests.cs
@@ -1,7 +1,134 @@
-namespace SabreTools.Help.Test
+using SabreTools.Help.Inputs;
+using Xunit;
+
+namespace SabreTools.Help.Test.Inputs
 {
     public class Int8InputTests
     {
+        [Fact]
+        public void ProcessInput_EmptyArgs_Failure()
+        {
+            string[] args = [];
+            int index = 0;
 
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.Equal(sbyte.MinValue, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_NegativeIndex_Failure()
+        {
+            string[] args = ["a", "5"];
+            int index = -1;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(-1, index);
+            Assert.Equal(sbyte.MinValue, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_OverIndex_Failure()
+        {
+            string[] args = ["a", "5"];
+            int index = 2;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(2, index);
+            Assert.Equal(sbyte.MinValue, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Space_InvalidLength_Failure()
+        {
+            string[] args = ["a"];
+            int index = 0;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.Equal(sbyte.MinValue, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Space_InvalidValue_Failure()
+        {
+            string[] args = ["a", "ANY"];
+            int index = 0;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.Equal(sbyte.MinValue, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Space_ValidValue_Success()
+        {
+            string[] args = ["a", "5"];
+            int index = 0;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.True(actual);
+            Assert.Equal(1, index);
+            Assert.Equal(5, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Equal_InvalidLength_Failure()
+        {
+            string[] args = ["a="];
+            int index = 0;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.Equal(sbyte.MinValue, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Equal_InvalidValue_Failure()
+        {
+            string[] args = ["a=ANY"];
+            int index = 0;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.False(actual);
+            Assert.Equal(0, index);
+            Assert.Equal(sbyte.MinValue, input.Value);
+        }
+
+        [Fact]
+        public void ProcessInput_Equal_ValidValue_Success()
+        {
+            string[] args = ["a=5"];
+            int index = 0;
+
+            var input = new Int8Input("a", "a", "a");
+            bool actual = input.ProcessInput(args, ref index);
+
+            Assert.True(actual);
+            Assert.Equal(0, index);
+            Assert.Equal(5, input.Value);
+        }
     }
 }
